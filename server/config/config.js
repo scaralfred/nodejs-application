f95d3db0b6adb143b856f4f3cc2554fed93d621b
scsaralfred <alfredoscarano87@gmail.com>
2018-04-21 22:28:26 +0800

    Added mongo DB

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://heroku_lh9phdbj:voi7mc1fdq60cg72tiu620u4v1@ds151169.mlab.com:51169/heroku_lh9phdbj/TodoApp';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
