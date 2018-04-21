8257ccd032c01599625d415a99d942909dcfb721
scsaralfred <alfredoscarano87@gmail.com>
2018-04-21 22:37:00 +0800

    New app with mongo updated

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://heroku_lh9phdbj:voi7mc1fdq60cg72tiu620u4v1@ds151169.mlab.com:51169/heroku_lh9phdbj/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://heroku_4kxr0m6x:o6iqditcgf7kkptdm49vb9r30o@ds113749.mlab.com:13749/heroku_4kxr0m6x/TodoApp';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
