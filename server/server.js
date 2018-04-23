db2fcff70acbc4833eb32ed94544657aaafcaa95
scsaralfred <alfredoscarano87@gmail.com>
2018-04-23 14:09:03 +0800

    Update to use JSON file for the development and heroku server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+// Require config file
 require('./config/config');
 
+// Required the installed npm packages +  MongoDB and Mongoos
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,15 +10,21 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
+
+// SET UP EXPRESS AND PORT NUMBER 
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+// POST /todos
+
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -26,22 +34,31 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+// GET ALL TODOS
+
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+      _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id', (req, res) => {
+// GET A SINGLE TODO
+
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+      _id: id,
+      _creator: req.user._id 
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -52,14 +69,19 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.delete('/todos/:id', (req, res) => {
+// DELETE A SINGLE TODO
+
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+     _id: id,
+     _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -70,7 +92,9 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-app.patch('/todos/:id', (req, res) => {
+// UPDATE A SINGLE TODO
+
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -85,7 +109,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -96,18 +120,54 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-app.post('/users', (req, res) => {
-  var user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
+/// POST /users
 
-  user.save().then((user) => {
-    res.send(user);
+app.post('/users', (req, res) => {
+//  var user = new User({
+//    email: req.body.email,
+//    password: req.body.password
+//  });   OR YOU CAN ALSO USE THE LODASH METHOD => Pick   
+var body = _.pick(req.body, ['email','password']);
+var user = new User(body); 
+
+  user.save().then(() => {
+     return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
+
+// GET ROUTE
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+});
+
+// POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+   var body = _.pick(req.body, ['email','password']);
+     
+    User.findByCredentials(body.email, body.password).then((user) => {
+       return user.generateAuthToken().then((token) => {   res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+// DELETE => Logout Users
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+      res.status(200).send();
+  }, () => {
+      res.status(400).send();
+  }) 
+});
+// Express Listen
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
