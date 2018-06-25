40e48c96b68ed7d884385525bcafa1d8b49b735b
scsaralfred <alfredoscarano87@gmail.com>
2018-06-25 23:34:23 +0800

    Updating

--- server/server.js
+++ server/server.js
@@ -139,7 +139,8 @@ var user = new User(body);
   user.save().then(() => {
      return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user);
+//    res.header('x-auth', token).send(user);
+    res('idToken', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
@@ -157,7 +158,9 @@ app.post('/users/login', (req, res) => {
    var body = _.pick(req.body, ['email','password']);
      
     User.findByCredentials(body.email, body.password).then((user) => {
-       return user.generateAuthToken().then((token) => {   res.header('x-auth', token).send(user);
+       return user.generateAuthToken().then((token) => {   
+           //    res.header('x-auth', token).send(user);
+                res('idToken', token).send(user);
         });
     }).catch((e) => {
         res.status(400).send();
