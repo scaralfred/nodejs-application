f03d26417bd1c6e07771ea735fefe563f5fc1b26
scsaralfred <alfredoscarano87@gmail.com>
2018-06-26 00:07:59 +0800

    CORS

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
     res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, x-auth, X-auth, X-Auth, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
     
     next();
